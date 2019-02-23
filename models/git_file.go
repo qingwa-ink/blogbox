996fa3f67789bbb323e5916fc64ef489a049eb11
lishaoqing <lishaoqing@mobimagic.com>
2019-02-23 17:01:02 +0800

    添加了Git文件实体类

---
+++ models/git_file.go
@@ -0,0 +1,14 @@
+package models
+
+// GitFile Github文件
+type GitFile struct {
+	Name         string `json:"name"`
+	Path         string `json:"path"`
+	Size         int    `json:"size"`
+	Sha          string `json:"sha"`
+	HTMLURL      string `json:"html_url"`
+	URL          string `json:"url"`
+	DownloadsURL string `json:"download_url"`
+	GitURL       string `json:"git_url"`
+	Type         string `json:"type"`
+}
