396f5596888e7c3f5108122cc42d37edc71259d7
lishaoqing <lishaoqing@mobimagic.com>
2019-02-23 17:00:15 +0800

    完成了下载文件夹和文件的功能

--- services/git_service.go
+++ services/git_service.go
@@ -3,8 +3,10 @@ package services
 import (
 	"blog/models"
 	"blog/tools"
+	"bufio"
 	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
@@ -24,7 +26,7 @@ func RefreshGitData() {
 	gitAPIURL := fmt.Sprintf("https://api.github.com/repos/%s", gitName)
 	body, err := tools.HTTPGet(gitAPIURL)
 	if err != nil {
-		logs.Warn("something wrong here: %s", err.Error())
+		logs.Error("Load Project Error : %s", err.Error())
 		return
 	}
 
@@ -45,12 +47,49 @@ func RefreshGitData() {
 
 func download(gitName, filePath, savePath string) {
 
+	path := fmt.Sprintf("%s/%s", savePath, filePath)
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Println("temp dir is not exist")
+			err := os.Mkdir(path, os.ModePerm)
+			if err != nil {
+				fmt.Printf("mkdir failed![%v]\n", err)
+			}
+		}
+	}
+
 	gitAPIURL := fmt.Sprintf("https://api.github.com/repos/%s/contents/%s", gitName, filePath)
 	body, err := tools.HTTPGet(gitAPIURL)
 	if err != nil {
-		logs.Warn("something wrong here: %s", err.Error())
+		logs.Error("Load Contents Error : %s", err.Error())
 		return
 	}
 
-	logs.Warn("body : %s", string(body))
+	var files []models.GitFile
+	json.Unmarshal(body, &files)
+
+	logs.Debug("body : %+v", files)
+
+	for _, file := range files {
+
+		if file.Type == "file" {
+			httpBody, err := tools.HTTPGet(file.DownloadsURL)
+			if err != nil {
+				logs.Error("Download File Error: %s", err.Error())
+				return
+			}
+			path := fmt.Sprintf("%s/%s", savePath, file.Path)
+			file, err := os.Create(path)
+			if err != nil {
+				logs.Error("Save File Error: %s", err.Error())
+				return
+			}
+			writer := bufio.NewWriter(file)
+			writer.Write(httpBody)
+			writer.Flush()
+		} else if file.Type == "dir" {
+			download(gitName, file.Path, savePath)
+		}
+	}
 }
