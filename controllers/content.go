023fb3ae603280258edb3db2ac3d29c7aab7a29d
lishaoqing <lishaoqing@mobimagic.com>
2019-02-28 14:08:35 +0800

    解决了html转义的问题

---
+++ controllers/content.go
@@ -0,0 +1,43 @@
+package controllers
+
+import (
+	"blog/models"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"os"
+
+	"github.com/astaxie/beego"
+	"github.com/russross/blackfriday"
+)
+
+type ContentController struct {
+	beego.Controller
+}
+
+func (c *ContentController) Get() {
+
+	var content models.BlogContent
+	var category models.BlogCategory
+	var project models.BlogProject
+
+	bps, _ := project.FindAll()
+
+	path := c.Input().Get("path")
+	content.FindContentByPath(path)
+
+	gitPath := beego.AppConfig.String("git_path")
+	mdPath := fmt.Sprintf("%s/%s", gitPath, path)
+	f, err := os.Open(mdPath)
+	if err != nil {
+		return
+	}
+	datas, _ := ioutil.ReadAll(f)
+
+	c.Data["Content"] = template.HTML(string(blackfriday.MarkdownCommon(datas)))
+	c.Data["Categories"], _ = category.FindCategories(0, "")
+	c.Data["Project"] = bps[0]
+	c.Data["Nickname"] = beego.AppConfig.String("nickname")
+	c.Data["Information"] = beego.AppConfig.String("information")
+	c.TplName = "content.html"
+}
---
+++ controllers/index.go
@@ -0,0 +1,29 @@
+package controllers
+
+import (
+	"blog/models"
+
+	"github.com/astaxie/beego"
+)
+
+type IndexController struct {
+	beego.Controller
+}
+
+func (c *IndexController) Get() {
+
+	var content models.BlogContent
+	var category models.BlogCategory
+	var project models.BlogProject
+
+	bps, _ := project.FindAll()
+
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["Contents"], _ = content.FindContents(1, "")
+	c.Data["Categories"], _ = category.FindCategories(0, "")
+	c.Data["Project"] = bps[0]
+	c.Data["Nickname"] = beego.AppConfig.String("nickname")
+	c.Data["Information"] = beego.AppConfig.String("information")
+	c.TplName = "index.html"
+}
