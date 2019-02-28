e2ccb6f4c3d627f3cfdae95b4b6b75d4f6dd4fe7
lishaoqing <lishaoqing@mobimagic.com>
2019-02-28 15:41:35 +0800

    添加了专栏过滤

--- controllers/index.go
+++ controllers/index.go
@@ -17,10 +17,15 @@ func (c *IndexController) Get() {
 	var project models.BlogProject
 
 	bps, _ := project.FindAll()
+	cat := c.Input().Get("cat")
 
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
-	c.Data["Contents"], _ = content.FindContents(1, "")
+	if cat == "" {
+		c.Data["Contents"], _ = content.FindContents(1, "")
+	} else {
+		c.Data["Contents"], _ = content.FindContents(1, cat)
+	}
 	c.Data["Categories"], _ = category.FindCategories(0, "")
 	c.Data["Project"] = bps[0]
 	c.Data["Nickname"] = beego.AppConfig.String("nickname")
